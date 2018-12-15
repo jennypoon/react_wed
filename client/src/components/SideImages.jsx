7a04a03666a3ae959470603513b96c3595137b02
Jenny Poon <jenny.poon@live.ca>
2018-12-14 16:40:46 -0800

    added component to for side image

---
+++ client/src/components/SideImages.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import image from '../images/cityline.jpeg'
+
+function SideImage() {
+    return (
+      <div className="sideimage">
+        <img src={image} alt="engagement - cityline"/>
+      </div>
+    )
+}
+
+export default SideImage;
