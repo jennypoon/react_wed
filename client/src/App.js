f35ad109ee3f91c5d804dbe881960de6ae6e8518
Jenny Poon <jenny.poon@live.ca>
2018-12-15 13:58:49 -0800

    added tabs feature for different sections of site

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
-import SideImages from "./components/SideImages.jsx"
+import SideImages from "./components/SideImages.jsx";
+import OurStory from "./components/OurStory.jsx";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "react-tabs/style/react-tabs.css";
 import './App.css';
 
 class App extends Component {
   render() {
+
+    const tabs = (
+      <div class="tabStyle">
+        <Tabs>
+          <TabList>
+            <Tab> Our Story </Tab>
+            <Tab> The Venue </Tab>
+            <Tab> Schedule </Tab>
+            <Tab> RSVP </Tab>
+          </TabList>
+
+          <TabPanel>
+            <OurStory />
+          </TabPanel>
+
+          <TabPanel>
+
+          </TabPanel>
+
+          <TabPanel>
+
+          </TabPanel>
+        </Tabs>
+      </div>
+      )
+
+
     return (
-      <SideImages />
+      <div>
+        <SideImages />
+        { tabs }
+      </div>
     );
   }
 }
---
+++ client/src/components/OurStory.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OurStory() {
+    return (
+        <div>
+        <h1>It all started at a bus stop</h1>
+        </div>
+    )
+}
+
+export default OurStory;
