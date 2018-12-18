71a70414320e7f5187b32525cd9c248da1cb1d5c
Jenny Poon <jenny.poon@live.ca>
2018-12-18 13:26:20 -0800

    required body parser and morgan

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
