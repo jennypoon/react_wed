48cc2dc5d0f5e71677b4012218937a2ec7285719
Jenny Poon <jenny.poon@live.ca>
2018-12-20 18:02:07 -0800

    removed connection object

--- knexfile.js
+++ knexfile.js
@@ -42,9 +42,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-    DATABASE_URL: 'postgres://clseknnnslksmv:c7138f5b9ed92a386ca864fd60ea2b2e11c9fc92fdc790dda5e6967cd5c27294@ec2-54-243-238-46.compute-1.amazonaws.com:5432/d5csc4353v6afc'
-    },
+    connection: 'postgres://clseknnnslksmv:c7138f5b9ed92a386ca864fd60ea2b2e11c9fc92fdc790dda5e6967cd5c27294@ec2-54-243-238-46.compute-1.amazonaws.com:5432/d5csc4353v6afc',
     pool: {
       min: 2,
       max: 10
