fafea07023856a8f7a42b3213f52bb50c40d854f
Jenny Poon <jenny.poon@live.ca>
2018-12-20 18:08:33 -0800

    revert back to .env for database url

--- knexfile.js
+++ knexfile.js
@@ -42,7 +42,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: 'postgres://clseknnnslksmv:c7138f5b9ed92a386ca864fd60ea2b2e11c9fc92fdc790dda5e6967cd5c27294@ec2-54-243-238-46.compute-1.amazonaws.com:5432/d5csc4353v6afc',
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
