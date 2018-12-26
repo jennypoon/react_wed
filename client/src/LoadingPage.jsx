a65075f2f5fb1d5beced5edec86f96b3d228e3f5
Jenny Poon <jenny.poon@live.ca>
2018-12-25 17:23:16 -0800

    add animation to loading text

--- client/src/LoadingPage.jsx
+++ client/src/LoadingPage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Countdown from "./components/Countdown.jsx";
-import logo from "./images/vista_logo.png";
+import logo from "./images/loadingPage.png";
 import "./styling/loadingpage.css";
 
 
@@ -9,9 +9,11 @@ class LoadingPage extends Component {
 
   render() {
     return (
-      <div>
-      <img className="logoLoading" src={logo} alt="JS logo" />
-      <Countdown />
+      <div className="loadingPage">
+        <h2 className="titleLoadingPage"> Jenny & Stephen Wedding </h2>
+        <h3 className="loadingText"> l   o   a   d   i   n   g . . . </h3>
+        <img className="logoLoading" src={logo} alt="JS logo" />
+        <Countdown />
       </div>
     )
   }
