4b73cf3853f8d467b90a96764b948baf0bf07c27
Jenny Poon <jenny.poon@live.ca>
2018-12-31 18:15:23 -0800

    added structure to component

--- client/src/components/Schedule.jsx
+++ client/src/components/Schedule.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import "../styling/schedule.css"
 
 class Schedule extends Component {
  render() {
   return (
-    <p> Details about Schedule </p>
+    <div class="timeline">
+      <div class="container left">
+        <div class="content">
+        <h2>2017</h2>
+        <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+      </div>
+    </div>
+    <div class="container right">
+      <div class="content">
+        <h2>2016</h2>
+        <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+      </div>
+    </div>
+  </div>
   )}
 }
 
