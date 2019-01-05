48502e1788507df329ea833394e8b32932fd9ab4
Jenny Poon <jenny.poon@live.ca>
2019-01-04 22:14:30 -0800

    added template for photos

--- client/src/components/Team.jsx
+++ client/src/components/Team.jsx
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import '../styling/team.css';
+import avatar from '../images/avatar.png'
 
 class Team extends Component {
  render() {
   return (
-    <p> Will soon be revealed </p>
+    <div className="teamWrapper">
+      <div className="imgWrapper">
+        <img src={avatar} className="indivImage"/>
+      </div>
+
+      <div className="imgWrapper">
+        <img src={avatar} className="indivImage"/>
+      </div>
+
+      <div className="imgWrapper">
+        <img src={avatar} className="indivImage"/>
+      </div>
+
+      <div className="imgWrapper">
+        <img src={avatar} className="indivImage"/>
+      </div>
+
+      <div className="imgWrapper">
+        <img src={avatar} className="indivImage"/>
+      </div>
+
+      <div className="imgWrapper">
+        <img src={avatar} className="indivImage"/>
+      </div>
+    </div>
   )}
 }
 
 export default Team;
+
