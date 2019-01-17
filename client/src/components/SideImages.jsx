6fc6abf0a2712b88be45dc212b2da40907a5f393
Jenny Poon <jenny.poon@live.ca>
2019-01-16 18:42:16 -0800

    added name header

--- client/src/components/SideImages.jsx
+++ client/src/components/SideImages.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 function SideImage() {
     return (
       <div className="imageSlider">
+        <div className="headerName">
+          <h2> Stephen & Jenny </h2>
+        </div>
         <div className="image1"></div>
         <div className="image2"></div>
         <div className="image3"></div>
